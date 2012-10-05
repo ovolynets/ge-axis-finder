c7e208a97975e035f4352763f40eaafb965eb7a0
alexjaguarvol <alexjaguarvol@gmail.com>
2012-10-05 14:40:56 +0200

    Added license and copyright information

--- proof/TMCOccSideSelector.C
+++ proof/TMCOccSideSelector.C
@@ -1,4 +1,27 @@
+// Copyright (C) 2009-2012
+//
+// Author: Oleksandr Volynets
+// email:  alexjaguarvol@gmail.com
+//
+// This is free software; you can redistribute it and/or modify it under
+// the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+//
+// This software is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+
 #define TMCOccSideSelector_cxx
+// The selector skeleton was automatically created by ROOT.
+// Here is the information from ROOT:
+
 // The class definition in TMCOccSideSelector.h has been generated automatically
 // by the ROOT utility TTree::MakeSelector(). This class is derived
 // from the ROOT class TSelector. For more information on the TSelector
--- proof/TMCOccSideSelector.h
+++ proof/TMCOccSideSelector.h
@@ -1,3 +1,26 @@
+// Copyright (C) 2009-2012
+//
+// Author: Oleksandr Volynets
+// email:  alexjaguarvol@gmail.com
+//
+// This is free software; you can redistribute it and/or modify it under
+// the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+//
+// This software is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+
+// The selector skeleton was automatically created by ROOT.
+// Here is the information from ROOT:
+
 //////////////////////////////////////////////////////////
 // This class has been automatically generated on
 // Tue Jun 21 21:07:28 2011 by ROOT version 5.28/00b
